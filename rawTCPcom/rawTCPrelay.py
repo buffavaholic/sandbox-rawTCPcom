223b5775c1c39fa6087f4707ea1f7a23b54ca37c
E. Greenwald <egreenwald@ucsd.edu>
2016-05-05 17:38:40 +0000

    ok actually commiting the files now

---
+++ rawTCPcom/rawTCPrelay.py
@@ -0,0 +1,86 @@
+from socket import socket, SO_REUSEADDR, SOL_SOCKET
+from asyncio import Task, coroutine, get_event_loop
+
+class Peer(object):
+    def __init__(self, server, sock, name):
+        self.loop = server.loop
+        self.name = name
+        self._sock = sock
+        self._server = server
+        if self.name[0] == '127.0.0.1':
+            self.global_name = 'com.opentrons.robot_to_tcp'
+        else:
+            self.global_name = 'com.opentrons.tcp_to_robot'
+        Task(self._peer_handler())
+
+    def send(self, data):
+        return self.loop.sock_sendall(self._sock, data.encode('utf8'))
+    
+    @coroutine
+    def _peer_handler(self):
+        try:
+            yield from self._peer_loop()
+        except IOError:
+            pass
+        finally:
+            self._server.remove(self)
+
+    @coroutine
+    def _peer_loop(self):
+        while True:
+            buf = yield from self.loop.sock_recv(self._sock, 400000)
+            if buf == b'':
+                break
+##            self._server.broadcast('%s: %s' % (self.name, buf.decode('utf8')))
+            #print("new msg")
+##            rawMsg = buf;
+##            msgIn = buf.decode('utf8')
+##            print(buf)
+##            self._server.broadcast('%s\t%s' % (self.global_name, buf.decode('utf8')))
+            self._server.relay('%s\t%s' % (self.global_name, buf.decode('utf8')),self._sock)       
+class Server(object):
+    def __init__(self, loop, port):
+        self.loop = loop
+        self._serv_sock = socket()
+        self._serv_sock.setblocking(0)
+        self._serv_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self._serv_sock.bind(('',port))
+        self._serv_sock.listen(5)
+        self._peers = []
+        Task(self._server())
+
+    def remove(self, peer):
+        self._peers.remove(peer)
+        self.broadcast('%s\tquit!\n\r' % (peer.global_name,))
+
+    def broadcast(self, message):
+##        print(list(self._peers))
+        print("sep msg")
+        for peer in self._peers:
+            peer.send(message)
+
+    def relay(self, message, incomingSock):
+##        print(list(set(incomingSock)-set(self._peers)))
+        
+        for peer in self._peers:
+            if peer._sock != incomingSock:
+                peer.send(message)
+
+    @coroutine
+    def _server(self):
+        while True:
+            peer_sock, peer_name = yield from self.loop.sock_accept(self._serv_sock)
+            peer_sock.setblocking(0)
+            peer = Peer(self, peer_sock, peer_name)
+            self._peers.append(peer)
+            self.broadcast('%s\tconnected!\n\r' % (peer.global_name,))
+##            print(peer_name[0]=='127.0.0.1')
+            #print(peer_sock)
+
+def main():
+    loop = get_event_loop()
+    Server(loop, 1234)
+    loop.run_forever()
+
+if __name__ == '__main__':
+    main()
